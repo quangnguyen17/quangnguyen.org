51b2bf73917c523866152697111dde645536b3a7
Quang nguyen <quangnguyen@Quangs-MBP.attlocal.net>
2022-07-22 23:54:53 -0700

    bew ucibs

--- src/SinglePage.tsx
+++ src/SinglePage.tsx
@@ -1,6 +1,19 @@
 import { FC } from 'react'
 import './SinglePage.css'
 
+const Banner = () => (
+  <div className="banner">
+    <p>
+      Have an app idea but don't know where to start? Want to work together? Let's build something
+      cool.{' '}
+      <a href="mailto:wan15112001@gmail.com" target="_blank">
+        Email me
+      </a>{' '}
+      and we'll chat.
+    </p>
+  </div>
+)
+
 const Header = () => (
   <header>
     <div className="heading">
@@ -28,10 +41,6 @@ const Bio = () => (
       automotive.
     </p>
     <p>
-      Have an app idea but don't know where to start? I can help with that, connect with me and
-      we'll chat.
-    </p>
-    <p>
       You can find my resume <a href="/resume.pdf">here</a>.
     </p>
   </div>
@@ -39,7 +48,7 @@ const Bio = () => (
 
 const Services = () => (
   <>
-    <h2 className="headline">Services I offer</h2>
+    <h2 className="headline">Services I Offer</h2>
     <div>
       <p>
         I offer software development consulting services to help you build the product of your dream
@@ -53,16 +62,16 @@ const Services = () => (
 
 const Connect = () => (
   <>
-    <h2 className="headline">Connect with me</h2>
+    <h2 className="headline">Connect With Me</h2>
     <div className="connect">
+      <a href="https://github.com/quangnguyen17" target="_blank">
+        <img className="github" src="github.png" alt="github.png" />
+      </a>
       <a href="https://twitter.com/quangnguyen1501" target="_blank">
         <img className="twitter" src="twitter.png" alt="twitter.png" />
       </a>
       <a href="https://www.linkedin.com/in/quangnguyen2001" target="_blank">
         <img className="linkedin" src="linkedin.png" alt="linkedin.png" />
-      </a>
-      <a href="https://github.com/quangnguyen17" target="_blank">
-        <img className="github" src="github.png" alt="github.png" />
       </a>
     </div>
   </>
@@ -79,6 +88,7 @@ const Footer = () => (
 
 export const SinglePage: FC = () => (
   <div className="App">
+    <Banner />
     <Header />
     <main>
       <Bio />
